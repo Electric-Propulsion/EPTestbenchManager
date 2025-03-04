a5fe09581a7934245d0dbc7241cf39ec1001e4e8
epadmin <epadmin@souder.ca>
2025-03-03 19:21:58 -0800

    basic test building blocks and apparatus config updates

---
+++ eptestbenchmanager/experiment_runner/experiment_segments/set.py
@@ -0,0 +1,27 @@
+from . import ExperimentSegment
+
+
+class Set(ExperimentSegment):
+
+
+    def configure(self, config: dict):
+        self.setpoint = config["setpoint"]
+        
+        self.commanded_vinstrument_setpoint = self._testbench_manager.connection_manager.virtual_instruments[config["commanded_vinstrument_setpoint"]]
+        
+        output_name = config.get("commanded_vinstrument_output", None)
+        if output_name is not None:
+            self.commanded_vinstrument_output = self._testbench_manager.connection_manager.virtual_instruments[output_name]
+        else:
+            self.commanded_vinstrument_output = None
+
+    def run(self) -> None:
+        
+        self.commanded_vinstrument_setpoint.command(True)
+        
+        if self.commanded_vinstrument_output is not None:
+            self.commanded_vinstrument_output.command(True)
+        
+
+    def generate_report(self):
+        pass
---
+++ eptestbenchmanager/experiment_runner/experiment_segments/step.py
@@ -0,0 +1,46 @@
+import logging
+from . import ExperimentSegment
+from numpy import linspace
+import time
+
+logger = logging.getLogger(__name__)
+
+
+class Step(ExperimentSegment):
+
+    def configure(self, config: dict):
+        self.min = config["min"]
+        self.max = config["max"]
+        self.num_steps = config["num_steps"]
+        self.step_delay = config["step_delay"]
+        self.commanded_vinstrument_setpoint = self._testbench_manager.connection_manager.virtual_instruments[config["commanded_vinstrument"]]
+        output_name = config.get("commanded_vinstrument", None)
+        if output_name is not None:
+            self.commanded_vinstrument_output = self._testbench_manager.connection_manager.virtual_instruments[output_name]
+        else:
+            self.commanded_vinstrument_output = None
+
+    def run(self) -> None:
+        self.commanded_vinstrument_setpoint.command(self.min)
+        if self.commanded_vinstrument_output is not None:
+            self.commanded_vinstrument_output.command(True)
+
+        values = [
+            float(value)
+            for value in list(
+                linspace(
+                    self.min, self.max, self.num_steps
+                )
+            )
+        ]
+
+        for value in self.interruptable(values):
+            logger.debug("%s: Setting to %s", self.name, value)
+            self.commanded_vinstrument_setpoint.command(float(value))
+            self.interruptable_sleep(self.step_delay)
+
+        if self.commanded_vinstrument_output is not None:
+            self.commanded_vinstrument_output.command(False)
+
+    def generate_report(self):
+        pass
